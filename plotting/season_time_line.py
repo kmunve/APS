19c80617e4ac228eff1cdd667e8bf13a62664ac4
kmunve <kmu@nve.no>
2016-06-15 10:33:04 +0200

    Made figure generation a function

--- plotting/season_time_line.py
+++ plotting/season_time_line.py
@@ -11,6 +11,23 @@ from data.season_dummy import seasonData
 __author__ = 'kmu'
 '''
 
+
+def generic_figure(title=None):
+    # Setting up the bokeh figure
+    fig = figure(title=title, x_axis_type="datetime", tools='crosshair')
+
+    # make the outline "invisible"
+    fig.outline_line_color = 'white'
+
+    # change just some things about the x-grid
+    fig.xgrid.grid_line_color = None
+
+    # change just some things about the y-grid
+    fig.ygrid.grid_line_alpha = 0.5
+    fig.ygrid.grid_line_dash = [6, 4]
+
+    return fig
+
 sd = seasonData()
 # check if you can filter out data: e.g. precip > 30 mm
 # reduces also the amount of data to be plotted
@@ -21,42 +38,17 @@ tags = [filtered_sd['Precipitation']]
 
 output_file("season_timeline.html")
 
-# Setting up the figure
-p1 = figure(title="Seasonal overview", x_axis_type="datetime", tools='hover')
 
-# make the outline "invisible"
-p1.outline_line_color = 'white'
-
-# change just some things about the x-grid
-p1.xgrid.grid_line_color = None
-
-# change just some things about the y-grid
-p1.ygrid.grid_line_alpha = 0.5
-p1.ygrid.grid_line_dash = [6, 4]
-
-
+p1 = generic_figure(title="Seasonal overview")
 p1.segment(x0=filtered_sd['Time'], y0=filtered_sd['Dummy-y'], x1=filtered_sd['Time'], y1=filtered_sd['Precipitation'], line_width=4)
 # alternatives are 'rect' or 'circle'+'segment'
 
 
-
-
-# Setting up the figure
-p2 = figure(x_axis_type="datetime", tools='hover')
-
-# make the outline "invisible"
-p2.outline_line_color = 'white'
-
-# change just some things about the x-grid
-p2.xgrid.grid_line_color = None
-
-# change just some things about the y-grid
-p2.ygrid.grid_line_alpha = 0.5
-p2.ygrid.grid_line_dash = [6, 4]
-
+# Surface hoar example plot
 mask = sd.data['SurfaceHoar'] > 0.0
 sh_sd = sd.data[mask]
 
+p2 = generic_figure()
 p2.inverted_triangle(x=sh_sd['Time'], y=sh_sd['SurfaceHoar'], size=10, color="#DE2D26")
 
 
