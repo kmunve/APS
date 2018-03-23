60873c81f012dacce80c9745d15ed37c532a239f
kmunve <kmu@nve.no>
2018-03-23 08:48:43 +0100

    Some minor edits.

--- aps/aps_io/tests/read_json.py
+++ aps/aps_io/tests/read_json.py
@@ -2,5 +2,9 @@ import json
 
 with open("../api/temperature_distribution_by_elevation.json") as fid:
     td = json.load(fid)
-    for i in td["Dates"][0]["Temperatures"]["ElevationBands"]:
-        print(i["Minimum"], i["FirstQuartile"], i["Median"], i["ThirdQuartile"], i["Maximum"])
+    for dt in td["Dates"]:
+        print(dt["Date"])
+        for reg in dt["Regions"]:
+            print(reg["RegionName"])
+            for eb in reg["ElevationBands"]:
+                print(eb["Minimum"], eb["FirstQuartile"], eb["Median"], eb["ThirdQuartile"], eb["Maximum"])
