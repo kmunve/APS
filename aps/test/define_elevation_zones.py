ae67c8cd938a7ec5ef123b5223b900e6cc391c00
kmunve <kmu@nve.no>
2018-04-09 22:16:35 +0200

    Some minor edits.

--- aps/test/define_elevation_zones.py
+++ aps/test/define_elevation_zones.py
@@ -9,13 +9,13 @@ import numpy as np
 import netCDF4
 import matplotlib.pyplot as plt
 
-reg_nc = netCDF4.Dataset(r"D:\Dev\APS\aps\data\terrain_parameters\VarslingsOmr_2017.nc", "r")
+reg_nc = netCDF4.Dataset(r"C:\Users\kmu\Dev\APS\aps\data\terrain_parameters\VarslingsOmr_2017.nc", "r")
 
 lok = reg_nc.variables["LokalOmr_2018"][:]
 reg = reg_nc.variables["VarslingsOmr_2017"][:]
 
 DEM = "MEANHeight"
-elev_nc = netCDF4.Dataset(r"D:\Dev\APS\aps\data\terrain_parameters\{0}.nc".format(DEM), "r")
+elev_nc = netCDF4.Dataset(r"C:\Users\kmu\Dev\APS\aps\data\terrain_parameters\{0}.nc".format(DEM), "r")
 
 elev = elev_nc.variables[DEM][:]\
 
@@ -23,7 +23,7 @@ elev_lok = np.where(lok == 4001, elev, np.nan)
 for i in range(90, 101):
     print("{0}%: {1}".format(i, np.nanpercentile(elev_lok.flatten(), i)))
 
-elev_reg = np.where(reg == 3007, elev, np.nan)
+elev_reg = np.where(reg == 3035, elev, np.nan)
 for i in range(90, 101):
     print("{0}%: {1}".format(i, np.nanpercentile(elev_reg.flatten(), i)))
 
